44e1f9c5fe6c381088d9a328156c3769679e74b4
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-04-23 12:40:37 +0800

    Initial commit

---
+++ edot.py
@@ -0,0 +1,12 @@
+'''
+filename: edot.py
+
+Takes in a file input, distinguishes odd from even, and then double or triple them.
+
+1. Take a file input
+2. Iterate through all the numbers from file input
+    - If even, then double and append to even.txt file
+    - If odd, then triple and append to odd.txt file
+3. Remember to close the txt file! (Use with operator for a foolproof execution!)
+
+'''
